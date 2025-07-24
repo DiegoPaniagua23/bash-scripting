5d566eb199ee77351ccd17f0bb6cdd3fb2004138
Diego Paniagua <diegopaniaguamolina@gmail.com>
2025-07-23 20:36:16 -0600

    Arrays example script

---
+++ scripts/arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+My_First_List=(one two three four five)
+
+echo ${My_First_List[0]}
+echo ${My_First_List[1]}
+echo ${My_First_List[2]}
+echo ${My_First_List[3]}
+echo ${My_First_List[4]}
+echo ${My_First_List[@]}
